96c7d6dbd1f97b5ff00d3fe3bd0d9c8b696685b7
Avinash Agarwal <73463300+walnashgit@users.noreply.github.com>
2025-01-01 18:29:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+CONFIG = {
+    # General Configuration
+    "root_dir": "/data",
+    "data_annotation_file": {
+        "train": "./data_annotations_train.csv",
+        "val": "./data_annotations_val.csv"
+    },
+    "num_classes": 1000,  # Number of prediction classes
+    "batch_size": 384, #256,  # Batch size for training
+    "epochs": 40,  # Total number of epochs
+    "learning_rate": 0.8, #1e-6,  # Initial learning rate
+    "momentum": 0.9,
+    "weight_decay": 1e-4,
+    "augment_prob": 0.25,  # Probability for augmentations (e.g., HorizontalFlip)
+
+    # Hardware Configuration
+    "gpus": 1,  # Number of GPUs to use
+    "precision": "16-mixed",  # FP16 mixed precision training
+
+    # DataLoader Configuration
+    "num_workers": 10,  # Number of DataLoader workers
+    "pin_memory": True,  # Use pinned memory for DataLoader
+
+    "check_val_every_n_epoch": 5,
+
+    "lr_finder": False
+}
