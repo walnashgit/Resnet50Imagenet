d81c3dd16f8ef2e8be82ecac0fce9f866fbb01c2
Avinash Agarwal <73463300+walnashgit@users.noreply.github.com>
2025-01-04 18:17:25 +0530

    Update create_annotations.py

--- create_annotations.py
+++ create_annotations.py
@@ -2,18 +2,28 @@
 import os
 import csv
 import json
-from util import annotation_file, train_dir, class_index_file, val_mapping_file, val_annotation_file, val_dir
+# from util import annotation_file, train_dir, class_index_file, val_mapping_file, val_annotation_file, val_dir
+from config import CONFIG
 
 # Initialize the class-to-index dictionary
 class_to_index = {}
+
+# Data paths from config
+annotation_file = CONFIG["data_annotation_file"]["train"]
+train_dir = CONFIG["train_dir"]
+class_index_file = CONFIG["class_index_file"]
+val_mapping_file = CONFIG["val_mapping_file"]
+val_annotation_file = CONFIG["data_annotation_file"]["val"]
+val_dir = CONFIG["val_dir"]
+
 
 
 # Create a CSV file
 def create_train_annotation():
     with open(annotation_file, mode='w', newline='') as file:
         writer = csv.writer(file)
-        # Write the header
-        writer.writerow(["image_path", "index", "class_name"])
+        # Write the header - Not needed
+        # writer.writerow(["image_path", "index"])
 
         # Walk through each class folder
         for class_index, class_name in enumerate(sorted(os.listdir(train_dir))):
@@ -26,7 +36,7 @@ def create_train_annotation():
                 for image_name in os.listdir(class_path):
                     image_path = os.path.join(class_path, image_name)
                     # Write image metadata to the CSV
-                    writer.writerow([image_path, class_index, class_name])
+                    writer.writerow([image_path, class_index])
 
     # Save the class-to-index dictionary to a JSON file
     with open(class_index_file, mode='w') as json_file:
@@ -34,6 +44,7 @@ def create_train_annotation():
 
     print(f"Metadata CSV file saved to {annotation_file}")
     print(f"Class-to-index dictionary saved to {class_index_file}")
+
 
 def create_val_annotation():
     # Load the class-to-index dictionary
@@ -46,8 +57,8 @@ def create_val_annotation():
         mapping_reader = csv.reader(mapping_file)
         writer = csv.writer(output_file)
 
-        # Write the header for the output CSV
-        writer.writerow(["image_path", "index", "class_name"])
+        # Write the header for the output CSV - Not needed
+        # writer.writerow(["image_path", "index"])
 
         # Iterate through the mapping file
         for row in mapping_reader:
@@ -59,7 +70,7 @@ def create_val_annotation():
             if class_name in class_to_index:
                 class_index = class_to_index[class_name]
                 # Write the annotation to the CSV
-                writer.writerow([full_image_path, class_index, class_name])
+                writer.writerow([full_image_path, class_index])
             else:
                 print(f"Warning: Class name '{class_name}' not found in class-to-index mapping.")
 
